22fb0176cb2320d2374ce4a19cc2bebe56ce3d10
Sorin ROSU <sorinrosu.pro@gmail.com>
2026-09-17 11:22:20 +0200

    update map_path

--- src/main.py
+++ src/main.py
@@ -1,35 +1,38 @@
 import os
-os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = '1'
+
+os.environ["PYGAME_HIDE_SUPPORT_PROMPT"] = "1"
 from parsing import Parsing  # noqa: E402
 from render import rendering  # noqa: E402
-from simulation import NoPathFound # noqa: E402
+from simulation import NoPathFound  # noqa: E402
 from pydantic import ValidationError  # noqa: E402
 from data import Data  # noqa: 402
 import sys  # noqa: E402
 
-RED = '\033[31m'
-RESET = '\033[0m'
+RED = "\033[31m"
+RESET = "\033[0m"
 
 if __name__ == "__main__":
     if len(sys.argv) == 2:
         map_p = sys.argv[1]
     else:
-        map_p = 'maps/droken/05_two_starts.txt'
+        map_p = "maps/easy/01_linear_path.txt"
     try:
         data: Data = Parsing().parse(map_p)
         rendering(data, map_p)
     except ValidationError as e:
-        word = 'errors' if len(e.errors()) > 1 else 'error'
-        print(f'{RED}{len(e.errors())} {word} occurred while attempting to'
-              f' parse {map_p}{RESET}')
-        print(f'  {RED}➜{RESET} ', e.errors()[0]['msg'])
+        word = "errors" if len(e.errors()) > 1 else "error"
+        print(
+            f"{RED}{len(e.errors())} {word} occurred while attempting to"
+            f" parse {map_p}{RESET}"
+        )
+        print(f"  {RED}➜{RESET} ", e.errors()[0]["msg"])
         sys.exit()
-    except ValueError as e:
+    except ValueError:
         sys.exit()
     except FileNotFoundError as e:
-        print(type(e).__name__, ':', e)
+        print(type(e).__name__, ":", e)
         sys.exit()
     except KeyboardInterrupt:
-        print('\nProgram closed successfully')
+        print("\nProgram closed successfully")
     except NoPathFound as e:
         print(e)
